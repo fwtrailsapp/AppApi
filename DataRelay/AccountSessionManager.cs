75d9e70d7712270b7a358afe7dc84d35cfb1aa47
libjared <libjared@users.noreply.github.com>
2016-04-02 00:59:52 -0400

    accountsessionmanager and logintoken

---
+++ DataRelay/AccountSessionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DataRelay
+{
+    public class AccountSessionManager
+    {
+        private readonly Dictionary<LoginToken, string> _loggedUsers;
+
+        public AccountSessionManager()
+        {
+            _loggedUsers = new Dictionary<LoginToken, string>();
+        }
+
+        public LoginToken Add(string userAccountId)
+        {
+            var token = new LoginToken();
+            _loggedUsers.Add(token, userAccountId);
+            return token;
+        }
+
+        public string GetAccountIdFromToken(LoginToken token)
+        {
+            string userAccountId;
+            if (_loggedUsers.TryGetValue(token, out userAccountId))
+            {
+                return userAccountId;
+            }
+            throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ DataRelay/LoginToken.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataRelay
+{
+    public class LoginToken
+    {
+        private readonly Guid token;
+
+        public LoginToken() : this(Guid.NewGuid())
+        {
+        }
+
+        public LoginToken(Guid token)
+        {
+            this.token = token;
+        }
+
+        protected bool Equals(LoginToken other)
+        {
+            return token.Equals(other.token);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((LoginToken)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return token.GetHashCode();
+        }
+    }
+}
