c41e0e2bfcda69bb99d0df6d169340d52805a6d3
libjared <libjared@users.noreply.github.com>
2016-05-01 02:57:19 -0400

    logintoken formatting and docs

--- DataRelay/LoginToken.cs
+++ DataRelay/LoginToken.cs
@@ -3,16 +3,25 @@ using System.Runtime.Serialization;
 
 namespace DataRelay
 {
+    /// <summary>
+    ///     A logintoken represents a session for the clients. Its underlying type is a Guid.
+    /// </summary>
     [DataContract]
     public class LoginToken
     {
-        [DataMember]
-        private readonly Guid token;
+        [DataMember] private readonly Guid token;
 
+        /// <summary>
+        /// Instantiates a new LoginToken. It is guaranteed to be unique.
+        /// </summary>
         public LoginToken() : this(Guid.NewGuid())
         {
         }
 
+        /// <summary>
+        /// Instantiates a new LoginToken with the given Guid.
+        /// </summary>
+        /// <param name="token">the existing Guid to create a LoginToken of</param>
         public LoginToken(Guid token)
         {
             this.token = token;
@@ -28,7 +37,7 @@ namespace DataRelay
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
-            return Equals((LoginToken)obj);
+            return Equals((LoginToken) obj);
         }
 
         public override int GetHashCode()
